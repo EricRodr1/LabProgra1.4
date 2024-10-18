da73d5c710cdc25a6ce60c39d1a7e4d162e61ce6
EricRodr1 <eric_rodriguez@unitec.edu>
2024-10-18 15:03:31 -0600

    Cuarto Commit

---
+++ src/main/java/com/mycompany/formulass/Formulass.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formulass;
+import java.util.Scanner;
+/**
+ *
+ * @author hyoyo
+ */
+public class Formulass {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in); 
+        
+        double problemaa,problemab,problemac,problemad,problemae,problemaf,problemag,problemah,problemai,problemaj,problemak;
+    int valorx=0, valory=0, valora=0, valorb=0, valorc=0, valord=0, valore=0, valorf=0, valorg=0, valorh=0, valorj=0, valorm=0, valorn=0, valorp=0, valorq=0, valorr=0, valors=0;
+    
+    System.out.println("Favor ingresar un valor para x:");
+    valorx= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para y:");
+    valory= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para a:");
+    valora= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para b:");
+    valorb= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para c:");
+    valorc= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para d:");
+    valord= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para e:");
+    valore= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para f:");
+    valorf= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para g:");
+    valorg= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para h:");
+    valorh= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para j:");
+    valorj= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para m:");
+    valorm= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para n:");
+    valorn= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para p:");
+    valorp= sc.nextInt();
+    System.out.println("Favor ingresar un valor para q:");
+    valorq= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para r:");
+    valorr= sc.nextInt();
+    
+    System.out.println("Favor ingresar un valor para s:");
+    valors= sc.nextInt();
+    
+    
+    problemaa=(3/2)+(4/3);
+    
+    problemab=(1/(valorx-5))-((3*valorx*valory)/4);
+    
+    problemac=(1/2)+7;
+    
+    problemad=7+(1/2);
+    
+    problemae=((valora^2)/(valorb-valorc))+((valord-valore)/(valorf-((valorg*valorh)/valorj)));
+    
+    problemaf=(valorm/valorn)+valorp;
+    
+    problemag=valorm+(valorn/(valorp-valorq));
+    
+    problemah=((valora^2)/(valorb^2))+((valorc^2)/(valord^2));
+    
+    problemai=(valorm+(valorn/valorp))/valorq-(valorr/valors);
+    
+    problemaj=3*valora+valorb/valorc-(valord+5*valore/(valorf+(valorg/(2*valorh))));
+    
+    problemak=(valora^2+(2*valora*valorb)+valorb^2)/((1/valorx^2)+2);
+    
+    
+    System.out.println("a)"+problemaa);
+    
+    System.out.println("b)"+problemab);
+    
+    System.out.println("c)"+problemac);
+    
+    System.out.println("d)"+problemad);
+    
+    System.out.println("e)"+problemae);
+    
+    System.out.println("f)"+problemaf);
+    
+    System.out.println("g)"+problemag);
+    
+    System.out.println("h)"+problemah);
+    
+    System.out.println("i)"+problemai);
+    
+    System.out.println("j)"+problemaj);
+    
+    System.out.println("k)"+problemak);
+    
+   
+}
+    
+}
+    
+
